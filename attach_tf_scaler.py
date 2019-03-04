863da753db38498ddf07fee92fd3c2f6f001181f
Artem Maevskiy <amaevskij@gmail.com>
2019-03-04 11:50:41 +0000

    attach tf script fix, gitignore updated

--- attach_tf_scaler.py
+++ attach_tf_scaler.py
@@ -1,11 +1,7 @@
 import os
-os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES']='1'
-
 
 import tensorflow as tf
 tf_config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
-
 
 from quantile_transformer_tf import QuantileTransformerTF
 
@@ -21,6 +17,8 @@ from make_prediction import DataSplits
 utils_rich = utils_rich_mrartemev
 particles = ['kaon', 'pion', 'proton', 'muon']
 
+os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES']='1'
 
 data_full = {
     particle : utils_rich.load_and_merge_and_cut(utils_rich.datasets[particle])
@@ -46,14 +44,18 @@ def tf_scalers_from_scaler(scaler, dtype=np.float64):
                                         dtype)
     return scaler_x_tf, scaler_y_tf
 
-
 for particle in particles:
+    print("Working on {}s".format(particle))
     predictor = tf.contrib.predictor.from_saved_model(
-                    os.path.join(models_path, model_name_format.format(particle))
+                    os.path.join(models_path, model_name_format.format(particle)),
+                    config=tf_config
                 )
+    print("Loaded predictor")
+
     scaler = joblib.load(
         os.path.join(preprocessors_path, preprocessor_name_format.format(particle))
     )
+    print("Loaded scaler")
     
     input_name, = [x.name for x in predictor.feed_tensors['x'].consumers()[0].outputs]
     output_name = predictor.fetch_tensors['dlls'].name
@@ -61,6 +63,7 @@ for particle in particles:
     mod_graph = tf.Graph()
     with mod_graph.as_default():
         tf_scaler_x, tf_scaler_y = tf_scalers_from_scaler(scaler)
+        print("Created tf scalers")
         
         input_tensor = tf.placeholder(dtype=tf.float64, shape=(None, len(utils_rich.raw_feature_columns)), name='x')
         scaled_input = tf.cast(tf_scaler_x.transform(input_tensor, False), dtype=tf.float32)
@@ -71,12 +74,15 @@ for particle in particles:
                 os.path.join(models_path, model_name_format.format(particle)),
                 input_map={input_name : scaled_input}
             )
+            print("Reloaded the model with input_map")
+
             scaled_output = mod_graph.get_tensor_by_name(output_name)
             output_tensor = tf_scaler_y.transform(tf.cast(scaled_output, dtype=tf.float64), True)
 
             sub_graph_def = tf.graph_util.convert_variables_to_constants(
                 sess, sess.graph_def, [output_tensor.op.name]
             )
+            print("Sub-graph created")
     
     reduced_graph = tf.Graph()
     with reduced_graph.as_default():
@@ -91,6 +97,7 @@ for particle in particles:
                     input_tensor : data_full[particle][utils_rich.raw_feature_columns].values
                 }
             )
+            print("Calculated predictions with the sub-graph")
             
             for i, col in enumerate(utils_rich.dll_columns):
                 data_full[particle]["predicted_{}".format(col)] = predictions[:,i]
@@ -101,6 +108,7 @@ for particle in particles:
                 inputs={"x": input_tensor},
                 outputs={"dlls": output_tensor}
             )
+            print("Exported the sub-graph model")
 
 
 
@@ -110,3 +118,4 @@ splits = {
 }
 
 pd.to_pickle(splits, 'predictions_with_tfScaler.pkl')
+print("Saved predictions to pickle")
